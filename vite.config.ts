35f5dd91b7cbb5ba31133ea28502f6da906be433
AlexRaihn <iogan.dead@yandex.ru>
2024-10-18 00:04:34 +0300

    dev commit

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
       '/api': {
         target: 'https://www.klerk.ru',
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
       },
     },
   },
