d088ba62dad41aa5d2b0ec491eb65b66c6b36f6e
AlexRaihn <iogan.dead@yandex.ru>
2024-10-17 23:52:02 +0300

    dev commit

--- src/stores/httpClient.ts
+++ src/stores/httpClient.ts
@@ -2,10 +2,5 @@ import axios from 'axios'
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_PROD_API ?? 'www.klerk.ru',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
+  mode: 'no-cors',
 })
