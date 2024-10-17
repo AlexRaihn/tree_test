408e3cab483245215e515428249861f640afd689
AlexRaihn <iogan.dead@yandex.ru>
2024-10-17 23:43:11 +0300

    dev commit

--- src/stores/httpClient.ts
+++ src/stores/httpClient.ts
@@ -3,6 +3,8 @@ import axios from 'axios'
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_PROD_API,
   headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
     'Cache-Control': 'no-cache',
     'Content-Type': 'application/x-www-form-urlencoded',
   },
